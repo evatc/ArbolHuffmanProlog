0b50e608136e1cf3b23ef732d34b615464e2921c
elenagarcia05 <157798696+elenagarcia05@users.noreply.github.com>
2025-01-02 19:04:40 +0100

    Update ArbolHuffman.pl

--- ArbolHuffman.pl
+++ ArbolHuffman.pl
@@ -23,3 +23,44 @@ peso(rama(I,D),PT) :-
     peso(I,PI),
     peso(D,PD),
     PT is PI + PD.
+
+
+%Traducción de carácter a código
+char_codigohuff(Codigo, Caracter) :-
+    mi_arbol(Arbol),
+    char_codigohuff_arbol(Codigo, Arbol, Caracter).
+
+char_codigohuff_arbol([], hoja(C,_), C).
+char_codigohuff_arbol([0|Codigo], rama(I,_), Caracter) :-
+    char_codigohuff_arbol(Codigo,I,Caracter).
+
+char_codigohuff_arbol([1|Codigo], rama(_,D), Caracter) :-
+    char_codigohuff_arbol(Codigo,D,Caracter).
+
+%Traducción de código a carácter
+codigohuff_char(Codigo, Caracter) :-
+    mi_arbol(Arbol),
+    codigohuff_char_arbol(Codigo, Arbol, Caracter).
+
+codigohuff_char_arbol([], hoja(C,_), C).
+codigohuff_char_arbol([0|Codigo],rama(I,_),Caracter) :-
+    codigohuff_char_arbol(Codigo,I,Caracter).
+
+codigohuff_char_arbol([1|Codigo],rama(_,D),Caracter) :-
+    codigohuff_char_arbol(Codigo,D,Caracter).
+
+
+%Traducción de string de carácteres a string de códigos
+texto_a_bits(Texto, StringBits) :-
+    atom_chars(Texto, ListaChars),
+    texto_a_bits_lista(ListaChars, Bits),
+    maplist(number_chars, Bits, ListaBitsChars),
+    flatten(ListaBitsChars, ListaBitsFlatten),
+    atomic_list_concat(ListaBitsFlatten, '', AtomicBits),
+    atom_string(AtomicBits,StringBits).
+
+texto_a_bits_lista([],[]).
+texto_a_bits_lista([A|ListaChars],Union) :-
+    char_codigohuff(Codigo,A),
+    append(Codigo,Bits,Union),
+    texto_a_bits_lista(ListaChars,Bits).
